f852d70c0f63026dc16b6525bb0ea60b5cd97757
AFuture <afuture0@outlook.com>
2025-08-19 01:21:11 +0800

    update

---
+++ Sources/LLMFlow/Types/Context/Context+Store.swift
@@ -0,0 +1,308 @@
+//
+//  Context+Store.swift
+//  swift-workflow
+//
+//  Created by AFuture on 2025-08-18.
+//
+
+
+struct ContextStorePath {
+    struct Key {
+        let strValue: String
+        let intValue: Int?
+        
+        init(strValue: String) {
+            if let intValue = Int(strValue) {
+                self.init(strValue: strValue, intValue: intValue)
+            } else {
+                self.init(strValue: strValue, intValue: nil)
+            }
+        }
+        
+        init(intValue: Int) {
+            self.init(strValue: "\(intValue)", intValue: intValue)
+        }
+        
+        init(strValue: String, intValue: Int?) {
+            self.strValue = strValue
+            self.intValue = intValue
+        }
+    }
+    
+    let keys: [Key]
+    
+    init(keys: [Key]) {
+        self.keys = keys
+    }
+}
+
+extension ContextStorePath: CustomStringConvertible {
+    var description: String {
+        self.keys.map(\.strValue).joined(separator: ".")
+    }
+}
+
+extension ContextStorePath: LosslessStringConvertible {
+    init(_ description: String) {
+        let keys = description.split(separator: ".").map { ContextStorePath.Key(strValue: String($0)) }
+        self.init(keys: keys)
+    }
+}
+
+extension ContextStorePath: Codable {
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let raw = try container.decode(String.self)
+        self.init(raw)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.description)
+    }
+}
+
+
+extension ContextStorePath: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        self.init(value)
+    }
+}
+
+extension ContextStorePath: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: Int) {
+        self.init(keys: [.init(intValue: value)])
+    }
+}
+extension ContextStorePath.Key: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        self.init(strValue: value)
+    }
+}
+
+extension ContextStorePath.Key: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: Int) {
+        self.init(intValue: value)
+    }
+}
+
+extension ContextStorePath: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: ContextStorePath.Key...) {
+        self.init(keys: elements)
+    }
+}
+
+
+// MARK: Collection
+
+extension Collection where Element == AnySendable, Index == Int {
+    subscript(key position: ContextStorePath.Key?) -> Element? {
+        guard let idx = position?.intValue else {
+            return nil
+        }
+        return self[idx]
+    }
+    
+    subscript(path path: ContextStorePath?) -> Element? {
+        guard let keys = path?.keys else { return nil }
+        
+        let (key, rest) = keys.separateFirst()
+        
+        guard let value = self[key: key] else {
+            return nil
+        }
+        
+        guard let rest, !rest.isEmpty else {
+            return value
+        }
+        
+        if let dic = value as? [String: Element] {
+            return dic[path: .init(keys: rest)]
+        } else if let list = value as? [Element] {
+            return list[path: .init(keys: rest)]
+        }
+        
+        return nil
+    }
+}
+
+extension MutableCollection where Element == AnySendable, Index == Int {
+    subscript(key position: ContextStorePath.Key?) -> Element? {
+        get {
+            guard let idx = position?.intValue else {
+                return nil
+            }
+            return self[idx]
+        }
+        set {
+            guard let idx = position?.intValue else {
+                return
+            }
+            return self[idx] = newValue
+        }
+    }
+    
+    subscript(keys keys: [ContextStorePath.Key]?) -> Element? {
+        get {
+            guard let keys else { return nil }
+            
+            let (key, rest) = keys.separateFirst()
+            
+            guard let value = self[key: key] else {
+                return nil
+            }
+            
+            guard let rest, !rest.isEmpty else {
+                return value
+            }
+            
+            if let dic = value as? [String: Element] {
+                return dic[keys: rest]
+            } else if let list = value as? [Element] {
+                return list[keys: rest]
+            }
+            
+            return nil
+        }
+
+        set {
+            guard let keys else { return }
+            
+            let (key, rest) = keys.separateFirst()
+            
+            guard let rest, !rest.isEmpty else {
+                self[key: key] = newValue
+                return
+            }
+            
+            if var dict = self[key: key] as? [String: Element] {
+                dict[keys: rest] = newValue
+                self[key: key] = dict
+            } else if var list = self[key: key] as? [Element] {
+                list[keys: rest] = newValue
+                self[key: key] = list
+            }
+        }
+    }
+    
+    subscript(path path: ContextStorePath?) -> Element? {
+        get {
+            self[keys: path?.keys]
+        }
+        
+        set {
+            self[keys: path?.keys] = newValue
+        }
+    }
+    
+}
+
+// MARK: Dictionary
+
+extension Dictionary where Value == AnySendable, Key == String {
+    subscript(safe key: Key?) -> Value? {
+        get {
+            guard let key else { return nil }
+            return self[key]
+        }
+        
+        set {
+            guard let key else { return }
+            self[key] = newValue
+        }
+    }
+    
+    subscript(key path: ContextStorePath.Key?) -> Value? {
+        get {
+            guard let key = path?.strValue, path?.intValue == nil else { return nil }
+            return self[key]
+        }
+        
+        set {
+            guard let key = path?.strValue, path?.intValue == nil else { return }
+            self[key] = newValue
+        }
+    }
+    
+    subscript(keys keys: [ContextStorePath.Key]?) -> Value? {
+        get {
+            guard let keys else { return nil }
+            
+            let (key, rest) = keys.separateFirst()
+            
+            guard let value = self[key: key] else {
+                return nil
+            }
+            
+            guard let rest, !rest.isEmpty else {
+                return value
+            }
+            
+            if let dic = value as? [Key: Value] {
+                return dic[path: .init(keys: rest)]
+            } else if let list = value as? [Value] {
+                return list[path: .init(keys: rest)]
+            }
+            
+            return nil
+        }
+        
+        set {
+            guard let keys else { return }
+            
+            let (key, rest) = keys.separateFirst()
+            
+            guard let rest, !rest.isEmpty else {
+                self[key: key] = newValue
+                return
+            }
+            
+            if var dict = self[key: key] as? [String: Value] {
+                dict[keys: rest] = newValue
+                self[key: key] = dict
+            } else if var list = self[key: key] as? [Value] {
+                list[keys: rest] = newValue
+                self[key: key] = list
+            }
+        }
+    }
+    
+    
+    subscript(path path: ContextStorePath?) -> Value? {
+        get {
+            self[keys: path?.keys]
+        }
+        
+        set {
+            self[keys: path?.keys] = newValue
+        }
+    }
+    
+    subscript(path keys: Key...) -> Value? {
+        get {
+            self[keys: keys.map {ContextStorePath.Key(strValue: $0)} ]
+        }
+        
+        set {
+            self[keys: keys.map {ContextStorePath.Key(strValue: $0)} ] = newValue
+        }
+    }
+    
+    subscript(path keys: [Key]?) -> Value? {
+        get {
+            self[keys: keys?.map { ContextStorePath.Key(strValue: $0) }]
+        }
+        
+        set {
+            self[keys: keys?.map { ContextStorePath.Key(strValue: $0) }] = newValue
+        }
+    }
+}
+
+
+
+extension Collection {
+    fileprivate func separateFirst() -> (Element?, [Element]?) {
+        (self.first, Array(self.dropFirst()))
+    }
+}
--- Tests/LLMFlowTests/StoreTests.swift
+++ Tests/LLMFlowTests/StoreTests.swift
@@ -12,17 +12,55 @@ import TestKit
 @testable import LLMFlow
 
 
-@Test("testStoreRead")
-func testStoreRead() async throws {
+@Test("testStoreSimpleRead")
+func testStoreSimpleRead() async throws {
     do {
         let store: [String: AnySendable] = ["foo": "bar"]
         #expect(store["foo"] as? String == "bar")
     }
     
     do {
+        let store: [AnySendable] = ["foo", "bar"]
+        
+        #expect(store[key: .init(intValue: 0)] as? String == "foo")
+        #expect(store[key: .init(intValue: 1)] as? String == "bar")
+    }
+}
+
+@Test("testStoreNestRead")
+func testStoreNestRead() async throws {
+    do {
         let store: [String: AnySendable] = ["foo": ["bar": "baz"]]
         #expect(store[path: ["foo", "bar"]] as? String == "baz")
     }
+    
+    do {
+        let store: [AnySendable] = [["foo", "bar"], ["baz", "raz"]]
+        #expect(store[path: [0, 0]] as? String == "foo")
+        #expect(store[path: [0, 1]] as? String == "bar")
+        #expect(store[path: [1, 0]] as? String == "baz")
+        #expect(store[path: [1, 1]] as? String == "raz")
+    }
+    
+    do {
+        let store: [String: AnySendable] = ["foo": ["bar", "baz"]]
+        #expect(store[path: ["foo"]] as? [String]? == ["bar", "baz"])
+        #expect(store[path: ["foo", 0]] as? String == "bar")
+        #expect(store[path: ["foo", 1]] as? String == "baz")
+    }
+    
+    do {
+        let store: [AnySendable] = [["foo": "bar"], ["baz":"raz"]]
+        #expect(store[path: [0]] as? [String: String]? == ["foo": "bar"])
+        #expect(store[path: [1]] as? [String: String]? == ["baz":"raz"])
+        #expect(store[path: [0, "foo"]] as? String == "bar")
+        #expect(store[path: [1, "baz"]] as? String == "raz")
+    }
+}
+
+
+@Test("testStoreSimpleModify")
+func testStoreSimpleModify() async throws {
     
     do {
         var store: [String: AnySendable] = ["foo": "bar"]
@@ -33,6 +71,19 @@ func testStoreRead() async throws {
     }
     
     do {
+        var store: [AnySendable] = ["foo", "bar"]
+        store[0] = "baz"
+        
+        #expect(store[key: .init(intValue: 0)] as? String != "foo")
+        #expect(store[key: .init(intValue: 0)] as? String == "baz")
+        #expect(store[key: .init(intValue: 1)] as? String == "bar")
+    }
+}
+
+
+@Test("testStoreModify")
+func testStoreModify() async throws {
+    do {
         var store: [String: AnySendable] = ["foo": ["bar": "baz"]]
         store["foo"] = "value 1"
         
@@ -42,9 +93,40 @@ func testStoreRead() async throws {
     do {
         var store: [String: AnySendable] = ["foo": ["bar": "baz"]]
         store[path: ["foo", "bar"]] = "value 1"
-        print(store)
         
         #expect(store[path: ["foo", "bar"]] as? String == "value 1")
     }
     
+    do {
+        var store: [AnySendable] = [["foo", "bar"], ["baz", "raz"]]
+        store[path: [0]] = "value 1"
+        
+        #expect(store[path: [0]] as? String == "value 1")
+        #expect(store[path: [1]] as? [String]? == ["baz", "raz"])
+    }
+    
+    do {
+        var store: [AnySendable] = [["foo": "bar"], ["baz": "raz"]]
+        store[path: [0]] = "value 1"
+        
+        #expect(store[path: [0]] as? String == "value 1")
+        #expect(store[path: [1]] as? [String: String]? == ["baz": "raz"])
+    }
+    
+    do {
+        var store: [AnySendable] = [["foo": "bar"], ["baz": "raz"]]
+        store[path: [0, "foo"]] = "value 1"
+        
+        #expect(store[path: [0]] as? [String: String]? == ["foo": "value 1"])
+        #expect(store[path: [0, "foo"]] as? String == "value 1")
+        #expect(store[path: [1]] as? [String: String]? == ["baz": "raz"])
+    }
+    
+    do {
+        var store: [String: AnySendable] = ["foo": ["bar", "baz"]]
+        store[path: ["foo", 0]] = "value 1"
+        
+        #expect(store[path: ["foo"]] as? [String]? == ["value 1", "baz"])
+        #expect(store[path: ["foo", 0]] as? String == "value 1")
+    }
 }
